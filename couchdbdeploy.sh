8b6c83c51a05ac17bd5871314fe8cdb90ea29a65
Anjay Ajodha <anajod@microsoft.com>
2016-11-14 12:59:38 -0800

    Replace config file

--- couchdbdeploy.sh
+++ couchdbdeploy.sh
@@ -4,5 +4,6 @@ apt-get -qq install -y couchdb
 git clone https://github.com/anjayajodha/hospitalrun-deployment.git
 curl -L https://raw.githubusercontent.com/HospitalRun/hospitalrun-frontend/master/script/initcouch.sh | sh 
 cd ./hospitalrun-deployment
+service couchdb stop
 cp local.ini /etc/couchdb/local.ini
-service couchdb restart
+service couchdb start
