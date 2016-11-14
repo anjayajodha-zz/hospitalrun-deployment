83a566177827ac7c0bf1a8a564ed086a854f3579
anajod <anajod@microsoft.com>
2016-11-14 14:21:16 -0800

    Updated custom script

--- couchdbdeploy.sh
+++ couchdbdeploy.sh
@@ -3,7 +3,6 @@
 cd /
 apt-get -qq update
 apt-get -qq install -y couchdb
-curl -L https://raw.githubusercontent.com/HospitalRun/hospitalrun-frontend/master/script/initcouch.sh | sh 
-service couchdb stop
+curl https://raw.githubusercontent.com/anjayajodha/hospitalrun-frontend/master/script/initcouch.sh | sh 
 curl -L https://raw.githubusercontent.com/anjayajodha/hospitalrun-deployment/master/local.ini -o /etc/couchdb/local.ini
-service couchdb start
+service couchdb restart
