c3f57d4320634bcc16da683706c55199e3491b0f
Anjay Ajodha <anajod@microsoft.com>
2016-11-14 12:58:05 -0800

    Cleaned up script

--- couchdbdeploy.sh
+++ couchdbdeploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 apt-get -qq install -y couchdb
-https://raw.githubusercontent.com/HospitalRun/hospitalrun-frontend/master/script/initcouch.sh | sh 
+git clone https://github.com/anjayajodha/hospitalrun-deployment.git
+cd ./hospitalrun-deployment
+curl -L https://raw.githubusercontent.com/HospitalRun/hospitalrun-frontend/master/script/initcouch.sh | sh 
