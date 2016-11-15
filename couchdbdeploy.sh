126d0ea506d50084bc8d2ecad4a859ff8c646e25
anajod <anajod@microsoft.com>
2016-11-14 16:03:42 -0800

    Changed script

--- couchdbdeploy.sh
+++ couchdbdeploy.sh
@@ -4,5 +4,7 @@ cd /
 apt-get -qq update
 apt-get -qq install -y couchdb
 curl https://raw.githubusercontent.com/anjayajodha/hospitalrun-frontend/master/script/initcouch.sh | sh 
+curl -X PUT http://couchadmin:test@localhost:5984/_config/couch_httpd_oauth/use_users_db -d '"true"'
+curl -X PUT http://couchadmin:test@localhost:5984/_users/org.couchdb.user:hradmin -d '{"name": "hradmin", "password": "test", "roles": ["System Administrator","admin","user"], "type": "user", "userPrefix": "p1"}'
 curl -L https://raw.githubusercontent.com/anjayajodha/hospitalrun-deployment/master/local.ini -o /etc/couchdb/local.ini
 service couchdb restart
